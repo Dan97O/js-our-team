f0b9d104fb43308b7ceb1b6efaf007f970be13c4
Dan97O <dan97-oddo@live.it>
2023-03-22 14:22:50 +0100

    add milestone 0, create array with info

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,11 +38,47 @@ Scott Estrada	Developer	scott-estrada-developer.jpg
 Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 */
 
+//const let 
+//Creare l’array di oggetti con le informazioni fornite.
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+//Stampare le stesse informazioni su DOM sottoforma di stringhe
+//Trasformare la stringa foto in una immagine effettiva
+//Organizzare i singoli membri in card/schede
 
 
-
-
-
-
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
+const membersTeam = [
+  {
+    name: 'Wayne Barnett',
+    role: 'Founder & CEO',
+    photo: 'wayne-barnett-founder-ceo.jpg',
+  },
+  {
+    name: 'Angela Caroll',
+    role: 'Chief Editor',
+    photo: 'angela-caroll-chief-editor.jpg',
+  },
+  {
+    name: 'Walter Gordon',
+    role: 'Office Manager',
+    photo: 'walter-gordon-office-manager.jpg',
+  },
+  {
+    name: 'Angela Lopez',
+    role: 'Social Media Manager',
+    photo: 'angela-lopez-social-media-manager.jpg',
+  },
+  {
+    name: 'Scott Estrada',
+    role: 'Developer',
+    photo: 'scott-estrada-developer.jpg',
+  },
+  {
+    name: 'Barbara Ramos',
+    role: 'Graphic Designer',
+    photo: 'barbara-ramos-graphic-designer.jpg',
+  },
+];
 
 
